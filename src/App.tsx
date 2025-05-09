0ea3f7403f4002d3a5d449da0fcf7759fe891050
Haikal Putrayudha <152383572+haikalllp@users.noreply.github.com>
2025-05-09 18:01:22 +0800

    Revert "Update src/App.tsx"
    This reverts commit 246f3b07a5787fabdbc4049fdc89d658a1580e38.

--- src/App.tsx
+++ src/App.tsx
@@ -114,13 +114,7 @@ const App = (): JSX.Element => {
 
   /* ============================== STATE: User Instructions ============================== */
   const [userInstructions, setUserInstructions] = useState('');
-  /**
-   * State variable used to trigger data re-fetching when its value changes.
-   * The `reloadTrigger` is incremented whenever a refresh of the file list or
-   * other related data is required. Components or hooks that depend on this
-   * state can listen for changes and re-execute their logic accordingly.
-   */
-  const [reloadTrigger, setReloadTrigger] = useState(0);
+  const [reloadTrigger, setReloadTrigger] = useState(0); // For triggering data re-fetch
   const lastSentIgnoreSettingsModifiedRef = useRef(null as boolean | null);
 
   // Utility function to clear all saved state and reset the app
